e8a9e59686dc1ea34a8ab3a647b8b00cd5cfc6e0
elias7alistair <falistair7@gmail.com>
2021-10-17 17:33:46 +0530

    d

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,31 @@
-import { io } from "socket.io-client";
 import "./App.css";
 import { useEffect, useState } from "react";
-function App() {
-  const [sockets, setSockets] = useState(null);
+import io from "socket.io-client";
+// socketIOClient is used in front-end to connect with socket backend url
+const socket = io.connect('localhost:5000');
 
+function App() {
   useEffect(() => {
-    
-    const socket = io(`localhost:5000`);
-    console.log(socket)
+    // Trigger JOIN_ROOM with unique room ID
+
+    // EMIT
+    // The Socket.IO API is inspired from the Node.js EventEmitter,
+    // which means you can emit events on one side and register listeners on the other:
+    // server-side- io.on("connection", (socket) => { socket.emit("hello", "world");
+
+    socket.emit("joinRoom", { username: "ali", room: "js" });
+  }, []);
+  useEffect(() => {
+    // Trigger 'NEW_MESSAGE' event
+    // Message received in the event NEW_MESSAGE
     socket.on("message", (message) => {
-      setSockets(message);
+      console.log(message);
     });
   }, []);
+ 
+  console.log(socket)
 
-  return <div>{sockets}</div>;
+  return <div>h</div>;
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-import express from "express";
-import path from "path";
 import http from "http";
-import socketio from "socket.io";
+import { Server } from "socket.io";
+import express from "express";
 import { formatMessage } from "./public/utils/messages.js";
 import cors from 'cors'
 import {
@@ -12,24 +11,21 @@ import {
 } from "./public/utils/user.js";
 
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
 const botName = "elias";
+// app.use(cors());
+const serverListner = http.createServer(app);
+var io = new Server(serverListner, { cors: { origin: '*' } });
 
-app.use(cors());
-
-//Set static folder
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "public")));
 
 // Run when a client connects
 io.on("connection", (socket) => {
   console.log('heah')
+  socket.emit("hello", "world"),
   socket.on("joinRoom", ({ username, room }) => {
     console.log('connected')
     const user = userJoin(socket.id, username, room);
     socket.join(user.room);
-    
+
     //welcome current user
     socket.emit("message", formatMessage(botName, "welcome to chat"));
 
@@ -71,6 +67,86 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 5000 || process.env.PORT;
+serverListner.listen(5000,()=>console.log(`${5000}`));
 
-server.listen(PORT, () => console.log(`server running on ${PORT}`));
+// import express from "express";
+// import path from "path";
+// import http from "http";
+// import socketio from "socket.io";
+// import { formatMessage } from "./public/utils/messages.js";
+// import cors from 'cors'
+// import {
+//   getCurrentUser,
+//   getRoomUsers,
+//   userJoin,
+//   userLeave,
+// } from "./public/utils/user.js";
+
+// const app = express();
+// const server = http.createServer(app);
+// var io = socketio(server,{
+//   cors: {
+//     origin: '*',
+//     methods: ['GET', 'POST']
+//   }
+// });
+// const botName = "elias";
+// app.use(cors());
+// app.options('*', cors());
+// // //Set static folder
+// // const __dirname = path.resolve();
+// // app.use(express.static(path.join(__dirname, "public")));
+
+// // Run when a client connects
+// io.on("connection", (socket) => {
+//   console.log('heah')
+//   socket.emit("hello", "world"),
+//   socket.on("joinRoom", ({ username, room }) => {
+//     console.log('connected')
+//     const user = userJoin(socket.id, username, room);
+//     socket.join(user.room);
+
+//     //welcome current user
+//     socket.emit("message", formatMessage(botName, "welcome to chat"));
+
+//     //Broadcast when a user connects
+//     socket.broadcast
+//       .to(user.room)
+//       .emit(
+//         "message",
+//         formatMessage(botName, `${username} has joined the chat`)
+//       );
+
+//     //send users and room info
+//     io.to(user.room).emit("roomUsers", {
+//       room: user.room,
+//       users: getRoomUsers(user.room),
+//     });
+//   });
+
+//   //listen for chatMessage
+//   socket.on("chatMessage", (msg) => {
+//     const user = getCurrentUser(socket.id);
+//     io.to(user.room).emit("message", formatMessage(user.username, msg));
+//   });
+
+//   // Runs when client disconncts
+//   socket.on("disconnect", () => {
+//     const user = userLeave(socket.id);
+//     if (user) {
+//       io.to(user.room).emit(
+//         "message",
+//         formatMessage(botName, `${user.username} has left the chat`)
+//       );
+
+//       io.to(user.room).emit("roomUsers", {
+//         room: user.room,
+//         users: getRoomUsers(user.room),
+//       });
+//     }
+//   });
+// });
+
+// const PORT = 6000 || process.env.PORT;
+
+// server.listen(PORT, () => console.log(`server running on ${PORT}`));
