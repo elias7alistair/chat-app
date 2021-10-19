367b1914f05374d2eb96e3d3bc1cf1ca28415338
elias7alistair <falistair7@gmail.com>
2021-10-19 21:53:00 +0530

    mobile and chat

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,7 +1,8 @@
+import e from "cors";
 import { useEffect, useState, useRef } from "react";
 import io from "socket.io-client";
 import styled from "styled-components/macro";
-
+import { Button } from "react-bootstrap";
 const socket = io.connect("https://alistairdevchat.herokuapp.com");
 
 const Chat = ({ match }) => {
@@ -53,8 +54,16 @@ const Chat = ({ match }) => {
         & > div {
           height: 100%;
         }
+        @media(max-width: 767px){
+          flex-direction: column;
+        }
       `}
     >
+      <MobilePanel>
+        <Title>Room Name</Title>
+        <SubHeader>Room {roomName}</SubHeader>
+   
+      </MobilePanel>
       <Panel>
         <Title>Room Name</Title>
         <SubHeader>Room {roomName}</SubHeader>
@@ -70,10 +79,12 @@ const Chat = ({ match }) => {
       <ChatRoom>
         <div
           css={`
-            overflow: scroll;
+            overflow-y: scroll;
             display: flex;
             flex-direction: column;
-            height: 100%
+            height: 100%;
+            margin-bottom: 27px;
+            padding: 18px;
           `}
         >
           {messages &&
@@ -81,11 +92,27 @@ const Chat = ({ match }) => {
               <ChatContainer isUser={message.username === userName}>
                 <div>{message.username}</div>
                 <div>{message.text}</div>
+                <div
+                  css={`
+                    position: absolute;
+                    right: 20px;
+                    font-size: 12px;
+                    opacity: 0.5;
+                    color: ${message.username === userName ? "white" : "black"};
+                  `}
+                >
+                  {message.username === userName ? "Sent: " : "Received: "}
+                  {message.time}
+                </div>
               </ChatContainer>
             ))}
           <div ref={messagesEndRef} />
         </div>
-        <ChatPannel>
+        <ChatPannel
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
           <input
             type="text"
             value={text}
@@ -95,7 +122,8 @@ const Chat = ({ match }) => {
             }}
           ></input>
           <button
-            onClick={() => {
+            onClick={(e) => {
+              e.preventDefault();
               text.length && socket.emit("chatMessage", text) && setText("");
             }}
           >
@@ -118,19 +146,35 @@ const SubHeader = styled.h2`
   text-align: -webkit-auto;
 `;
 
-const ChatPannel = styled.div`
+const MobilePanel = styled.div`
+top: 0;
+position: sticky;
+z-index: 999;
+background: #f9f9f9;\
+max-height: 78px;
+@media(min-width: 767px){
+  display: none;
+}
+`
+const ChatPannel = styled.form`
   & input {
     width: 100%;
+    border-radius: 0;
   }
   & button {
-    width: 100%;
+    width: 36%;
+    color: #fff;
+    border-radius: 0;
+    background-color: #6c757d;
+    border-color: #6c757d;
   }
 
+  height: 48px;
   display: flex;
   width: 100%;
-  position: fixed;
-  bottom: 0;
-  margin: 10px -15px;
+  // position: fixed;
+  // bottom: 0;
+  // margin: 10px -15px;
   border-top: 1px solid;
 `;
 const ChatContainer = styled.div`
@@ -142,6 +186,7 @@ padding 10px;
 width 79%;
 margin: 10px 0;
 align-self: ${(props) => props.isUser && "flex-end"};
+position:relative;
 `;
 const ChatRoom = styled.div`
   width: 100%;
@@ -154,4 +199,7 @@ const Panel = styled.div`
   background: #fefbf3;
   width: 350px;
   padding: 15px;
+  @media(max-width: 767px){
+    display: none;
+  }
 `;
